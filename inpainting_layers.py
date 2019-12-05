7f42eb745734d1aee8e216c59d34b2e3e988ed0e
koshian2 <saltcoffeehorse@gmail.com>
2019-12-05 11:59:15 +0900

    add pconv

--- inpainting_layers.py
+++ inpainting_layers.py
@@ -136,3 +136,130 @@ def upsampling2d_tpu(inputs, scale=2):
     x = K.repeat_elements(inputs, scale, axis=1)
     x = K.repeat_elements(x, scale, axis=2)
     return x
+
+# https://github.com/MathiasGruber/PConv-Keras
+class PConv2D(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size, strides=1, dilation_rate=1, activation=None,
+                 use_bias=True, kernel_initializer='glorot_uniform', kernel_regularizer=None,
+                 bias_initializer='zeros', bias_regularizer=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.dilation_rate = dilation_rate
+        self.activation = tf.keras.activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = tf.keras.initializers.get(kernel_initializer)
+        self.kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
+        self.bias_initializer = tf.keras.initializers.get(bias_initializer)
+        self.bias_regularizer = tf.keras.regularizers.get(bias_regularizer)
+
+    def build(self, input_shape):        
+        """Adapted from original _Conv() layer of Keras        
+        param input_shape: list of dimensions for [img, mask]
+        """
+                    
+        if input_shape[0][-1] is None:
+            raise ValueError('The channel dimension of the inputs should be defined. Found `None`.')
+            
+        self.input_dim = input_shape[0][-1]
+        
+        # Image kernel
+        kernel_shape = (self.kernel_size, self.kernel_size, self.input_dim, self.filters)
+        self.kernel = self.add_weight(shape=kernel_shape,
+                                      initializer=self.kernel_initializer,
+                                      name='img_kernel',
+                                      regularizer=self.kernel_regularizer)
+        # Mask kernel
+        self.kernel_mask = K.ones(shape=kernel_shape)
+
+        # Calculate padding size to achieve zero-padding
+        self.pconv_padding = (
+            (int((self.kernel_size-1)/2), int((self.kernel_size-1)/2)), 
+            (int((self.kernel_size-1)/2), int((self.kernel_size-1)/2)), 
+        )
+
+        # Window size - used for normalization
+        self.window_size = self.kernel_size ** 2        
+        
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.filters,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer)
+        else:
+            self.bias = None
+        self.built = True
+
+    def call(self, inputs):
+        '''
+        We will be using the Keras conv2d method, and essentially we have
+        to do here is multiply the mask with the input X, before we apply the
+        convolutions. For the mask itself, we apply convolutions with all weights
+        set to 1.
+        Subsequently, we clip mask values to between 0 and 1
+        ''' 
+
+        # Both image and mask must be supplied
+        if type(inputs) is not list or len(inputs) != 2:
+            raise Exception('PartialConvolution2D must be called on a list of two tensors [img, mask]. Instead got: ' + str(inputs))
+
+        # Padding done explicitly so that padding becomes part of the masked partial convolution
+        images = K.spatial_2d_padding(inputs[0], self.pconv_padding)
+        masks = K.spatial_2d_padding(inputs[1], self.pconv_padding)
+
+        # Apply convolutions to mask
+        mask_output = K.conv2d(
+            masks, self.kernel_mask, 
+            strides=self.strides,
+            padding='valid',
+            dilation_rate=self.dilation_rate
+        )
+
+        # Apply convolutions to image
+        img_output = K.conv2d(
+            (images*masks), self.kernel, 
+            strides=self.strides,
+            padding='valid',
+            dilation_rate=self.dilation_rate
+        )        
+
+        # Calculate the mask ratio on each pixel in the output mask
+        mask_ratio = self.window_size / (mask_output + 1e-8)
+
+        # Clip output to be between 0 and 1
+        mask_output = K.clip(mask_output, 0, 1)
+
+        # Remove ratio values where there are holes
+        mask_ratio = mask_ratio * mask_output
+
+        # Normalize iamge output
+        img_output = img_output * mask_ratio
+
+        # Apply bias only to the image (if chosen to do so)
+        if self.use_bias:
+            img_output = K.bias_add(
+                img_output,
+                self.bias)
+        
+        # Apply activations on the image
+        if self.activation is not None:
+            img_output = self.activation(img_output)
+            
+        return [img_output, mask_output]
+
+    def get_config(self):
+        base_config = super().get_config()
+        current_config = {
+            "filters" : self.filters,
+            "kernel_size" : self.kernel_size,
+            "strides" : self.strides,
+            "dilation_rate" : self.dilation_rate,
+            "activation" : self.activation,
+            "use_bias" : self.use_bias,
+            "kernel_initializer" : self.kernel_initializer,
+            "kernel_regularizer" : self.kernel_regularizer,
+            "bias_initializer" : self.bias_initializer,
+            "bias_regularizer" : self.bias_regularizer            
+        }
+        return {**base_config, **current_config}
